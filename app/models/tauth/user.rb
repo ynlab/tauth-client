ea6ce54305a2c2199b9ef859a539702707097ecd
Keita Urashima <ursm@ursm.jp>
2012-05-23 22:05:41 +0900

    Revert "Mongoid に対応したのではないか"
    This reverts commit 452a8f1d1b0f66aadc4908fd65e9941f491f942d.

--- app/models/tauth/user.rb
+++ app/models/tauth/user.rb
@@ -1,24 +1,5 @@
 module Tauth
-  if defined?(ActiveRecord)
-    class User < ActiveRecord::Base
-      attr_accessible :display_name, :email, :group_ids, :openid_identifier
-    end
-  elsif defined?(Mongoid)
-    class User
-      include Mongoid::Document
-
-      identity :type => Integer
-
-      field :display_name
-      field :email
-      field :group_ids
-      field :openid_identifier
-
-      class << self
-        def find_or_initialize_by_id(id)
-          find_or_initialize_by(:id => id)
-        end
-      end
-    end
+  class User < ActiveRecord::Base
+    attr_accessible :display_name, :email, :group_ids, :openid_identifier
   end
 end
