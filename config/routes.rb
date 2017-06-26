6342e7a8daf6eeaf8b3afb4fce7dac3abcf510d1
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2017-06-26 18:35:25 +0900

    logout は get じゃなきゃダメだった

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Tauth::Engine.routes.draw do
   root :to => 'sessions#new'
 
   match '/login', to: 'sessions#new', via: :get
-  match '/logout', to: 'sessions#destroy', via: :post
+  match '/logout', to: 'sessions#destroy', via: :get
   match '/provider/callback', to: 'sessions#create', via: :post
   match '/failure', to: 'sessions#failure', via: :get
 end
